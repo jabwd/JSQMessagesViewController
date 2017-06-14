460612910e44ab59d91a160c4c3f92cd532b629d
Antwan van Houdt <jabwd@exurion.com>
2017-06-14 14:43:53 +0200

    Moved up the placeholder in the inputToolbar

--- JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
+++ JSQMessagesViewController/Views/JSQMessagesComposerTextView.m
@@ -221,7 +221,7 @@
     if ([self.text length] == 0 && self.placeHolder) {
         [self.placeHolderTextColor set];
 
-        [self.placeHolder drawInRect:CGRectInset(rect, 7.0f, 5.0f)
+        [self.placeHolder drawInRect:CGRectInset(rect, 12.0f, 5.0f)
                       withAttributes:[self jsq_placeholderTextAttributes]];
     }
 }
